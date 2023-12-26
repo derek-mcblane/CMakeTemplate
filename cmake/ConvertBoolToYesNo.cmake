bda2d17f880b4fc487d69a73202d3734974e8715
Derek McBlane <mcblanederek@gmail.com>
2023-12-26 14:51:18 -0500

    fixup ConvertBoolToYesNo.cmake

--- cmake/ConvertBoolToYesNo.cmake
+++ cmake/ConvertBoolToYesNo.cmake
@@ -1,15 +1,13 @@
 include(CheckParseArguments)
 
-function(convert_bool_to_yes_no)
+function(convert_bool_to_yes_no BOOL_VALUE)
     set(PREFIX ${CMAKE_CURRENT_FUNCTION})
-    set(BOOL_VALUE ${ARGV0})
-    cmake_parse_arguments(PARSE_ARGV 1 ${PREFIX} "" "OUTPUT_VARIABLE" "")
+    cmake_parse_arguments(${PREFIX} "" "OUTPUT_VARIABLE" "" ${ARGN})
     check_parse_arguments(${PREFIX})
-
-    # TODO, this is unset in the parent scope.
     if (${BOOL_VALUE})
-        set(${PREFIX}_OUTPUT_VARIABLE "YES")
+        set(OUTPUT_VALUE "YES")
     else()
-        set(${PREFIX}_OUTPUT_VARIABLE "NO")
+        set(OUTPUT_VALUE "NO")
     endif()
+    set(${${PREFIX}_OUTPUT_VARIABLE} ${OUTPUT_VALUE} PARENT_SCOPE)
 endfunction()
